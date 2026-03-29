7eba8e1d69525f68a180691ef09de9634acbf20a
sayys <simonnnk16@gmail.com>
2026-03-29 16:49:33 +0200

    MixinWorldGenMinable is now deterministic

--- src/main/java/sayys/depthsupdate/mixin/MixinWorldGenMinable.java
+++ src/main/java/sayys/depthsupdate/mixin/MixinWorldGenMinable.java
@@ -1,6 +1,8 @@
 package sayys.depthsupdate.mixin;
 
+import java.util.Random;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,15 +12,13 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(WorldGenMinable.class)
 public abstract class MixinWorldGenMinable {
     @ModifyVariable(method = "generate", at = @At("HEAD"), argsOnly = true, ordinal = 0)
-    private @NonNull BlockPos depthsupdate$offsetModdedOres(@NonNull BlockPos pos) {
+    private @NonNull BlockPos depthsupdate$offsetModdedOres(@NonNull BlockPos pos, World worldIn, Random rand) {
         int y = pos.getY();
 
-        if (y >= 0 && y <= 64) {
-            if (Math.random() < 0.5) {
-                int newY = -64 + (int) (Math.random() * (y + 64 + 1));
+        if (y >= 0 && y <= 64 && rand.nextFloat() < 0.5f) {
+            int newY = -64 + rand.nextInt(y + 65);
 
-                return new BlockPos(pos.getX(), newY, pos.getZ());
-            }
+            return new BlockPos(pos.getX(), newY, pos.getZ());
         }
 
         return pos;
