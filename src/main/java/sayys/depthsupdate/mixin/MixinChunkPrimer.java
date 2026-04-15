c54046efe7d7e7940f2c9555842edaacd9c9f365
sayys <simonnnk16@gmail.com>
2026-04-15 02:55:54 +0200

    use getBlockState() instead of directly accessing the data array in ChunkPrimer.findGroundBlockIdx for better compat

--- src/main/java/sayys/depthsupdate/mixin/MixinChunkPrimer.java
+++ src/main/java/sayys/depthsupdate/mixin/MixinChunkPrimer.java
@@ -2,14 +2,11 @@ package sayys.depthsupdate.mixin;
 
 import sayys.depthsupdate.core.HeightContext;
 import sayys.depthsupdate.core.HeightManager;
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.chunk.ChunkPrimer;
 import org.jspecify.annotations.NonNull;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,10 +16,6 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(value = ChunkPrimer.class, priority = 2000)
 public abstract class MixinChunkPrimer {
     private static final IBlockState DEPTHSUPDATE_DEFAULT_STATE = Blocks.AIR.getDefaultState();
-
-    @Shadow
-    @Final
-    private char[] data;
 
     @ModifyConstant(method = "<init>", constant = @Constant(intValue = 65536))
     private int depthsupdate$expandDataArrays(int original) {
@@ -39,15 +32,15 @@ public abstract class MixinChunkPrimer {
     @Inject(method = "findGroundBlockIdx", at = @At("HEAD"), cancellable = true)
     private void depthsupdate$findGroundBlockIdx(int x, int z, @NonNull CallbackInfoReturnable<Integer> cir) {
         HeightContext ctx = HeightManager.getMaxContext();
-        int yBitShift = ctx.yBitShift();
         int minY = ctx.minY();
+        ChunkPrimer self = (ChunkPrimer) (Object) this;
 
         for (int y = ctx.maxY() - 1; y >= minY; --y) {
-            int idx = (x << (yBitShift + 4)) | (z << yBitShift) | (y - minY);
-            IBlockState state = Block.BLOCK_STATE_IDS.getByValue(this.data[idx]);
+            IBlockState state = self.getBlockState(x, y, z);
 
             if (state != null && state != DEPTHSUPDATE_DEFAULT_STATE) {
                 cir.setReturnValue(y);
+
                 return;
             }
         }
